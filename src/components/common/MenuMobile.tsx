984eb0c144c98865961dc1598d7e8e6e6849ac33
Stefex <g.ste94@hotmail.it>
2026-03-18 23:47:03 +0100

    fix link

--- src/components/common/MenuMobile.tsx
+++ src/components/common/MenuMobile.tsx
@@ -14,7 +14,7 @@ export const MenuMobile = () => {
 
 <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-neutral-primary-soft bottom-0 left-1/2 menu-black lg:hidden">
     <div className="grid h-full max-w-lg grid-cols-5 mx-auto ">
-        <button data-tooltip-target="tooltip-home" type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-neutral-secondary-medium group">
+        <Link href="/"><button data-tooltip-target="tooltip-home" type="button" className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-neutral-secondary-medium group">
             <svg className="w-6 h-6 mb-1 text-body group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor"  d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/></svg>
             <span className="sr-only">Home</span>
         </button>
@@ -22,6 +22,7 @@ export const MenuMobile = () => {
             Home
             <div className="tooltip-arrow" data-popper-arrow></div>
         </div>
+        </Link>
         <Link href="/">
         <button data-tooltip-target="tooltip-wallet" type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-neutral-secondary-medium group">
             <svg className="w-6 h-6 mb-1 text-body group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" d="M17 8H5m12 0a1 1 0 0 1 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1Z"/></svg>
