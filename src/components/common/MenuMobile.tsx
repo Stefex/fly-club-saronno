524365234d4b2e1ba077a2eddf340a678bec5efd
Stefex <g.ste94@hotmail.it>
2026-03-19 00:35:36 +0100

    fix

--- src/components/common/MenuMobile.tsx
+++ src/components/common/MenuMobile.tsx
@@ -18,7 +18,7 @@ export const MenuMobile = () => {
 <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-neutral-primary-soft bottom-0 left-1/2 menu-black lg:hidden">
     <div className="grid h-full max-w-lg grid-cols-5 mx-auto ">
         
-          <Link href="/" className={`menu-item ${pathname === ("/") ? "active" : ""}`}>
+          <Link href="/" className={`menu-item first-item ${pathname === ("/") ? "active" : ""}`}>
           <button data-tooltip-target="tooltip-home" type="button" className="inline-flex flex-col items-center justify-center">
               <svg className="w-6 h-6 mb-1 text-body group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor"  d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/></svg>
               <span >Home</span>
