eb38d03808802145db8c43dc6e3f81f6fba8b0a6
Stefex <g.ste94@hotmail.it>
2026-03-19 22:06:38 +0100

    fix

--- src/components/ui/images/ImgStoria.tsx
+++ src/components/ui/images/ImgStoria.tsx
@@ -8,7 +8,7 @@ export default function ImgStoria() {
         <Image
           src="/images/img/club_foto.jpg"
           alt="Cover"
-          className="w-full border border-gray-200 rounded-xl dark:border-gray-800"
+          className="w-full rounded-xl dark:border-gray-800"
           width={1054}
           height={600}
         />
