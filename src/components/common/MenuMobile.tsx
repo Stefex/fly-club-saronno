1453ca684c1366ef5ed304e28e924b7604eeaced
Stefex <g.ste94@hotmail.it>
2026-03-19 10:11:57 +0100

    fix grafici menu

--- src/components/common/MenuMobile.tsx
+++ src/components/common/MenuMobile.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Badge from "../ui/badge/Badge";
-import { ArrowDownIcon,  ArrowRightIcon, CalenderIcon ,BoxIconLine, GroupIcon, DocsIcon } from "@/icons";
+import { ArrowDownIcon,  ArrowRightIcon,  CalenderIcon ,BoxIconLine, GroupIcon, DocsIcon, GridIcon } from "@/icons";
 import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import Button from "@/components/ui/button/Button"; 
@@ -20,18 +20,14 @@ export const MenuMobile = () => {
         
           <Link href="/" className={`menu-item first-item ${pathname === ("/") ? "active" : ""}`}>
           <button data-tooltip-target="tooltip-home" type="button" className="inline-flex flex-col items-center justify-center">
-              <svg className="w-6 h-6 mb-1 text-body group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor"  d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"/></svg>
+              <GridIcon />
               <span >Home</span>
           </button>
-          <div id="tooltip-home" role="tooltip" className="absolute z-10 invisible inline-block text-sm font-medium text-white rounded-base shadow-xs opacity-0 tooltip">
-              Home
-              <div className="tooltip-arrow" data-popper-arrow></div>
-          </div>
           </Link>
         
           <Link className={`menu-item ${pathname === ("/consiglio-direttivo") ? "active" : ""}`} href="/consiglio-direttivo">
           <button data-tooltip-target="tooltip-profile" type="button" className="inline-flex flex-col items-center justify-center">
-              <svg className="w-6 h-6 mb-1 text-body group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor"  d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/></svg>
+              <GroupIcon  />
               <span >Consiglio</span>
           </button>
           
